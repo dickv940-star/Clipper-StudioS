b21b5254ac305c8627089cc079198b3ce8dbb7f6
dickv940-star <dickv940@gmail.com>
2026-07-13 21:48:25 +0700

    Create state.js

---
+++ js/core/state.js
@@ -0,0 +1,21 @@
+const State = {
+
+    media: [],
+
+    tracks: [],
+
+    selectedClip: null,
+
+    currentMedia: null,
+
+    currentTime: 0,
+
+    duration: 0,
+
+    zoom: 1,
+
+    playing: false
+
+};
+
+export default State;
