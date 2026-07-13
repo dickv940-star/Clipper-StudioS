da948a443054d9d54bd2827adcdff9b0be50d817
dickv940-star <dickv940@gmail.com>
2026-07-13 21:32:48 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 import Uploader from "./core/uploader.js";
 import Player from "./core/player.js";
+import Timeline from "./core/timeline.js";
 
-window.addEventListener("DOMContentLoaded", () => {
-
-    Uploader.init();
+window.addEventListener("DOMContentLoaded",()=>{
 
     Player.init();
+
+    Timeline.init();
+
+    Uploader.init();
 
 });
