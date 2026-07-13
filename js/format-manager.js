6fa347c1fbf325747d367428424ea958b413698a
dickv940-star <dickv940@gmail.com>
2026-07-14 01:26:41 +0700

    Update format-manager.js

--- js/format-manager.js
+++ js/format-manager.js
@@ -1,19 +1,45 @@
-/*
-=========================================
-ClipperStudio
-Format Manager
-Version : 1.0
-
-Export Format Preset Controller
-
-=========================================
-*/
-
-
 const FormatManager = {
 
 
-    current:null,
+formats:[
+
+"mp4",
+"webm",
+"mov"
+
+],
 
 
-   
+
+getFormats(){
+
+
+return this.formats;
+
+
+},
+
+
+
+validate(format){
+
+
+return this.formats.includes(
+format
+);
+
+
+}
+
+
+
+};
+
+
+
+window.FormatManager = FormatManager;
+
+
+console.log(
+"Format Manager Ready"
+);
