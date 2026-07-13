65c42cb9bf700ddbdf10016b7e22628bea3fda0f
dickv940-star <dickv940@gmail.com>
2026-07-14 01:34:00 +0700

    Update delete.js

--- js/delete.js
+++ js/delete.js
@@ -1,41 +1,186 @@
 /*
-================================
+=================================
 
 ClipperStudio
-Export Manager
+Delete Engine
 
-================================
+=================================
 */
 
 
 (function(){
 
 
+"use strict";
 
-const ExportManager={
+
+
+if(window.Delete){
+
+    return;
+
+}
+
+
+
+const Delete = {
+
+
+
+    active:null,
+
+
+
+    callbacks:{},
+
+
 
 
 
     init(){
 
+
         console.log(
-            "Export Manager Ready"
+            "Delete Engine Ready"
         );
+
 
     },
 
 
 
-    async start(project){
 
 
-        console.log(
-            "Export Start",
-            project
+    select(clip){
+
+
+        this.active=clip;
+
+
+    },
+
+
+
+
+
+    execute(clip){
+
+
+        clip =
+
+        clip ||
+
+        this.active;
+
+
+
+        if(!clip){
+
+            console.warn(
+                "Tidak ada clip"
+            );
+
+            return false;
+
+        }
+
+
+
+        this.emit(
+
+            "delete",
+
+            clip
+
         );
 
 
+
         return true;
+
+
+    },
+
+
+
+
+
+    removeFromTrack(
+        track,
+        clip
+    ){
+
+
+        if(
+            !track ||
+            !track.clips
+        ){
+
+            return false;
+
+        }
+
+
+
+        const index=
+
+        track.clips.indexOf(
+            clip
+        );
+
+
+
+        if(index===-1){
+
+            return false;
+
+        }
+
+
+
+        track.clips.splice(
+            index,
+            1
+        );
+
+
+
+        return true;
+
+
+    },
+
+
+
+
+
+    on(
+        event,
+        callback
+    ){
+
+        this.callbacks[event]=callback;
+
+
+    },
+
+
+
+
+
+    emit(
+        event,
+        data
+    ){
+
+
+        if(
+            this.callbacks[event]
+        ){
+
+            this.callbacks[event](data);
+
+        }
 
 
     }
@@ -46,13 +191,12 @@ const ExportManager={
 
 
 
-window.ExportManager=
-ExportManager;
+window.Delete=Delete;
 
 
 
 console.log(
-"Export Manager Loaded"
+"Delete Engine Loaded"
 );
 
 
