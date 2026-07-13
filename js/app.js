a400d91691feaefd6053f002d4b8d567c87a164f
dickv940-star <dickv940@gmail.com>
2026-07-13 21:29:27 +0700

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+import Uploader from "./core/uploader.js";
+import Player from "./core/player.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    Uploader.init();
+
+    Player.init();
+
+});
