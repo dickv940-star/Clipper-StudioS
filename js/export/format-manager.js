daac3f3da1c6fc4581563ca2f2b0d28b6c4366fc
dickv940-star <dickv940@gmail.com>
2026-07-13 23:49:38 +0700

    Create format-manager.js

---
+++ js/export/format-manager.js
@@ -0,0 +1,19 @@
+/*
+=========================================
+ClipperStudio
+Format Manager
+Version : 1.0
+
+Export Format Preset Controller
+
+=========================================
+*/
+
+
+const FormatManager = {
+
+
+    current:null,
+
+
+   
